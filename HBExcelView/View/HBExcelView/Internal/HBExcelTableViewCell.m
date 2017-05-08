cd1b3339351df206e8cfa3f9991a233efeb0c8ef
tiny <tiny@huoban.com>
2017-05-08 18:49:34 +0800

    another way for the tableview not receive didSelectRowAtIndexPath.but still has problems for long press.

--- HBExcelView/View/HBExcelView/Internal/HBExcelTableViewCell.m
+++ HBExcelView/View/HBExcelView/Internal/HBExcelTableViewCell.m
@@ -11,6 +11,46 @@
 
 static void *kHBScrollViewContentOffset = &kHBScrollViewContentOffset;
 
+/**
+ * to resolve tableview not receive touching while a cell with a scrollview inside.
+ * still has problem for long press, to be determine.
+ */
+@interface HBExelTableViewCellScrollView : UIScrollView
+
+@end
+
+@implementation HBExelTableViewCellScrollView
+
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
+    if (!self.dragging)
+        [self.superview touchesCancelled: touches withEvent:event];
+    else
+        [super touchesCancelled: touches withEvent: event];
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    if (!self.dragging)
+        [self.superview touchesMoved: touches withEvent:event];
+    else
+        [super touchesMoved: touches withEvent: event];
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    if (!self.dragging)
+        [self.superview touchesBegan: touches withEvent:event];
+    else
+        [super touchesBegan: touches withEvent: event];
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    if (!self.dragging)
+        [self.superview touchesEnded: touches withEvent:event];
+    else
+        [super touchesEnded: touches withEvent: event];
+}
+
+@end
+
 @interface HBExcelTableViewCell ()
 
 @property (nonatomic, strong) UIView *fixedView;
